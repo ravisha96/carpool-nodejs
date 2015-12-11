b9c32b8f5b2389d21738df83dea6ca03b82a611f
ravi kumar sha <ravi.sha@nagarro.com>
2015-12-11 10:12:16 +0530

    testing

---
+++ src/backend/server.js
@@ -0,0 +1,22 @@
+/// <reference path="../../typings/tsd.d.ts" />
+define(["require", "exports", 'express'], function (require, exports, express) {
+    var viewRenderingEngine = require('ejs-mate');
+    var app = express();
+    app.engine('html', viewRenderingEngine.renderFile);
+    app.set('view engine', 'html'); // so you can render('index') 
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+    app.get('/api/coolresource', function (req, res) {
+        res.send({
+            "isApiWorking": false,
+            "name": "Maximilian Alexander"
+        });
+    });
+    var port = process.env.PORT || 3000;
+    var server = app.listen(port, function () {
+        var listeningPort = server.address().port;
+        console.log('The server is listening on port: ' + listeningPort);
+    });
+});
+//# sourceMappingURL=server.js.map
---
+++ src/backend/server.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import express = require('express');
+import path = require('path');
+var viewRenderingEngine = require('ejs-mate');
+
+var app : express.Express = express();
+
+app.engine('html', viewRenderingEngine);
+app.set('view engine', 'html'); // so you can render('index') 
+
+app.get('/', (req, res) => {
+	res.render('index');  
+}); 
+
+app.get('/api/coolresource', (req, res) => {
+	res.send({
+		"isApiWorking": false,
+		"name": "Maximilian Alexander"
+	});
+});
+
+var port: number = process.env.PORT || 3000;
+var server = app.listen(port, () => {
+	var listeningPort: number = server.address().port;
+	console.log('The server is listening on port: ' + listeningPort);
+});
